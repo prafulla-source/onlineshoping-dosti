cc929ab4c640b958b1aa1121678cbad4223cf6b4
prafulla-source <prafulladhal.sp@gmail.com>
2026-04-25 14:30:18 +0530

    initial commit - selenium project'

---
+++ src/test/java/day1/FirstTestCase.java
@@ -0,0 +1,23 @@
+package day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/*
+ *-lunch brower 
+ * Open URL
+ *  validate title shoube 'your store"
+ *  close brower
+ * */
+
+public class FirstTestCase {
+
+	public static void main(String[] args) {
+	   //lunch the brower (chrom)	
+		//ChromeDriver driver=new ChromeDriver();
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://www.flipkart.com/");
+		driver.findElement(By.name("q")).sendKeys("mens jeans");
+	}
+}
